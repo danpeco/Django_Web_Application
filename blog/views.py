5024f031b7f2b721ffd19419d53956621347b987
danpeco <dperez88@gmail.com>
2021-11-09 16:41:48 -0500

    Second commit

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'DanielPC',
-        'title': 'Blog post 1',
-        'content': 'Content of first post',
-        'date_posted': 'today'
-    },
-    {
-        'author': 'Julio',
-        'title': 'Blog post 2',
-        'content': 'Content of second post',
-        'date_posted': 'tomorrow'
-    }
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
